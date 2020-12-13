39dfa4f1ad147f9df7991ad90f5f5cfbd92dce99
Xavier Belanche <xbelanch@protonmail.com>
2020-12-13 20:56:42 +0100

    Here be Poggers

--- 01-C11/main.c
+++ 01-C11/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void parseInput(char *filename)
 {
-    printf("Hello world\n");
+    printf("Input filename: %s\n", filename);
+    // Open, Read and close the input file
+    FILE *fp;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        exit(-1);
+    }
+
+    char ch;
+    while ((ch = fgetc(fp)) != EOF) {
+        // print first number
+        printf("%c", ch);
+        if (ch == '\n') break;
+    }
+
+    fclose(fp);
+}
+
+int main(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i++) {
+        parseInput(argv[i]);
+    };
+
+    return 0;
 }
