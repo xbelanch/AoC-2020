32033d4056288f8125ae9b4728ee98caf78c064e
Xavier Belanche <xbelanch@protonmail.com>
2020-12-13 23:03:48 +0100

    [01]: Add solution for Day 1

--- 01-C11/main.c
+++ 01-C11/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void parseInput(char *filename)
+int numbers[999];
+
+int parseInput(char *filename)
 {
     int i = 0;
-    int numbers[999];
     int number;
+    /* int* tmp; */
 
     // Open, Read and close the input file
     FILE *file;
@@ -20,16 +22,43 @@ void parseInput(char *filename)
         numbers[i++] = number;
     }
 
-    printf("We have at the moment %d numbers on %s", i, filename);
+    // we remove all the null entries of the array
+    /* tmp = realloc(numbers, i*sizeof(int)); */
+    /* free(numbers); */
+    /* numbers = tmp; */
 
     fclose(file);
+
+    return i;
 }
+
+// Brute force!
+void calculateNumber(int length)
+{
+    printf("We have at the moment %d numbers\n", length);
+    for (int i = 0; i < length; i++) {
+        int entry = numbers[i];
+        for (int j = 0; j < length ; j++) {
+            if ((entry + numbers[j]) == 2020 && (entry != numbers[j])) {
+                printf("The two entries that sum to 2020 are %d and %d\n", entry, numbers[j]);
+                printf("I get %d if I multiply them together\n", entry * numbers[j]);
+                break;
+            }
+        }
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
+    int length;
+
     for (int i = 1; i < argc; i++) {
-        parseInput(argv[i]);
+        length = parseInput(argv[i]);
     };
+
+    calculateNumber(length);
+
 
     return 0;
 }
