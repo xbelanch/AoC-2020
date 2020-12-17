07fd1c818cce5b954db1f71b0e03ab5f1e4894b5
Xavier Belanche <xbelanch@protonmail.com>
2020-12-17 21:14:34 +0100

    [04]: Count the number of passwords from input file

--- 04-C11/main.c
+++ 04-C11/main.c
@@ -1,18 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXLINEBUFFER 2048
 #define log(string) printf("> %s", string)
 
+typedef struct passport {
+    int byr;
+    int iyr;
+    int eyr;
+    int hgt;
+    char* hcl;
+    char* ecl;
+    unsigned long pid;
+    int cid;
+} Passport;
+
+Passport* newPassword() {
+    Passport* passport = malloc(sizeof(Passport));
+
+    passport->byr = -1;
+    passport->iyr = -1;
+    passport->eyr = -1;
+    passport->hgt = -1;
+    passport->hcl = NULL;
+    passport->ecl = NULL;
+    passport->pid = 0;
+    passport->cid = -1;
+
+    return passport;
+}
+
 void parseInput(char* filename) {
-    puts(filename);
+    printf("%s:\n", filename);
 
     FILE* file = fopen(filename, "r");
     char* line = malloc(sizeof(char) * MAXLINEBUFFER);
 
+    int passports = 0;
     while ( fgets(line, MAXLINEBUFFER, file) != NULL) {
-        log(line);
+        // Check for dos and unix EOL format
+        // I'm not sure if this works under linux
+        if (line[0] != '\r' && line[0] != '\n') {
+            // log(line);
+        } else {
+            passports++;
+        }
     }
+    passports++; // count EOL as the last password of the input file
+    printf("Number of passports: %d\n", passports);
 }
 
 int main(int argc, char *argv[])
