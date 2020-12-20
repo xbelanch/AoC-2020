a948d6d10b247d2d1cff64fe65b4ecf4e8c9488a
Xavier Belanche <xbelanch@protonmail.com>
2020-12-20 10:17:58 +0100

    [04]: Add solution for part one of Day 4

--- 04-C11/main.c
+++ 04-C11/main.c
@@ -6,7 +6,7 @@
 #define MAXBUFFER 2048
 
 // https://stackoverflow.com/questions/35178520/how-to-read-parse-input-in-c-the-faq
-void parseInput(char* filename) {
+int parseInputDayOne(char* filename) {
 
     FILE* fp = fopen(filename, "rb");
 
@@ -14,29 +14,57 @@ void parseInput(char* filename) {
         fprintf(stderr, "Cannot open %s: %s\n", filename, strerror(errno));
 
     char* buffer = malloc(sizeof(char) * MAXBUFFER);
-    int line = 0;
-    int newline = 0;
+    int newline = 1;
+    char space = ' ';
+    int match = 0;
+    int valid = 0;
 
-    printf("Passport %d: \n", newline + 1);
     while ( fgets(buffer, MAXBUFFER, fp) != NULL) {
 
-        if (buffer[0] == '\r' && buffer[1] == '\n' ) {
-            newline++;
-            printf("Passport %d: \n", newline + 1);
-            continue;
+        if (buffer[0] != '\r' && buffer[1] != '\n' ) {
+            // Truncate lines
+            for (char *p = strtok(buffer, "\r"); p; p = strtok(NULL, "\r")) {
+                strncat(p, &space, 1);
+                for (char *q = strtok(p, "\r"); q; q = strtok(NULL, "\r")) {
+                    for (char *r = strtok(q, " "); r; r = strtok(NULL, " ")) {
+                        /* printf("%s\n", r); */
+                        if (strncmp("byr", r, 3) == 0 ||
+                            strncmp("iyr", r, 3) == 0 ||
+                            strncmp("eyr", r, 3) == 0 ||
+                            strncmp("hgt", r, 3) == 0 ||
+                            strncmp("hcl", r, 3) == 0 ||
+                            strncmp("ecl", r, 3) == 0 ||
+                            strncmp("pid", r, 3) == 0) match++;
+                        // CID field is optional, so we dont count whatever if
+                        // present or not
+                        // if (strncmp("cid", r, 3) == 0
+                    }
+                }
+            }
         } else {
-            printf("%d>%s", line, buffer);
-            line++;
+            /* printf("Match: %d\n", match); */
+            if (match == 7) valid++;
+            match = 0;
+            newline++;
+            /* printf("\nPassport %d: \n", newline + 1); */
+            continue;
         }
     }
+    // last passport of the input
+    /* printf("Match fields: %d\n", match); */
+    if (match == 7) valid++;
 
     fclose(fp);
+
+    printf("How many passports parsed? %d\n", newline);
+    return valid;
 }
 
 int main(int argc, char *argv[])
 {
     for (int i = 1; i < argc; i++) {
-        parseInput(argv[i]);
+        int partOne = parseInputDayOne(argv[i]);
+        printf("How of them are valid? %d\n", partOne);
     }
     return 0;
 }
