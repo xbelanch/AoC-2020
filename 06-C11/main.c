0adb1a35294f147c1460ec54e1f67eb163ee6870
Xavier Belanche <xbelanch@protonmail.com>
2020-12-27 11:57:59 +0100

    [06]: Solve sample-01 for part one

--- 06-C11/main.c
+++ 06-C11/main.c
@@ -4,13 +4,20 @@
 
 
 #define MAXLINEBUFFER 128
+#define MAXGROUPS 512
 
 typedef struct inputFile {
     int lines;
     char **input;
 } InputFile;
 
+typedef struct group {
+    int people;
+    char *questions;
+} Group;
+
 InputFile g_inputFile;
+Group* g_groups[512];
 
 void getInputData(char *filename)
 {
@@ -55,17 +62,89 @@ void getInputData(char *filename)
         }
         *dst = '\0';
 
-        g_inputFile.input[currentLineIndex];
     }
 
     fclose(fp);
 }
 
-void printInput() {
-    for (int i = 0; i < g_inputFile.lines; i++) {
-        printf("%d> %s", i, g_inputFile.input[i]);
+void createGroup(int people, char *questions, int index) {
+    Group *group = malloc(sizeof(Group));
+    group->people = people;
+    group->questions = questions;
+    if (g_groups[index] != NULL) {
+        g_groups[index] = group;
+    } else {
+        printf("ERROR: Cannot add new group at index %d\n", index);
     }
 }
+
+int checkNewGroup(char* string) {
+    int result = 0;
+    if (string[0] == '\n') {
+        result = 1;
+    }
+    return result;
+}
+
+
+void printInput() {
+    for (int i = 0; i < g_inputFile.lines; i++) {
+        if (checkNewGroup(g_inputFile.input[i])) {
+            printf("New group here\n");
+        } else {
+            printf("%d> %s", i, g_inputFile.input[i]);
+        }
+    }
+}
+
+int collectQuestions(char *questions) {
+
+    int occurrences = 0;
+    char *match = malloc(sizeof(char) * MAXLINEBUFFER);
+    match[0] = '\0';
+    for (char* ptr = questions; *ptr != '\0'; ptr++) {
+        for (char* src = match; *src != '\0'; src++) {
+            if (*ptr == *src) {
+                occurrences++;
+            }
+        }
+        if (occurrences == 0 || strlen(match) == 0)  {
+            size_t len = strlen(match);
+            char *tmp = malloc(sizeof(char) * len + 1 + 1); // one for extra char, one for trailing zero
+            tmp[len] = *ptr;
+            tmp[len + 1] = '\0';
+            strcpy(match, tmp);
+            free(tmp);
+        } else {
+            occurrences = 0;
+        }
+    }
+
+    printf("match> %s\n", match);
+    return strlen(match);
+}
+
+
+int partOne() {
+    /* int count = 0; */
+    /* int people = 0; */
+
+    // concat all the questions in one line
+    char *questions = malloc(sizeof(char) * MAXLINEBUFFER);
+    questions[0] = '\0';
+
+    for (int i = 0; i < g_inputFile.lines; i++) {
+        printf("%s", g_inputFile.input[i]);
+        strcat(questions, g_inputFile.input[i]);
+        // remove return carriages
+        char* ptr = questions + strlen(questions) - 1;
+        *ptr = '\0';
+    }
+    printf("%s\n", questions);
+    int nquestions = collectQuestions(questions);
+    printf("%d\n", nquestions);
+}
+
 
 
 int main(int argc, char *argv[])
@@ -73,6 +152,7 @@ int main(int argc, char *argv[])
     for (int i = 1; i < argc ; i++) {
         getInputData(argv[i]);
     }
-    printInput();
+    /* printInput(); */
+    partOne();
     return 0;
 }
