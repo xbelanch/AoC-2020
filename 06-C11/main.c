6de247fc4de7b37c25184986b23f1fb25f9b95d3
Xavier Belanche <xbelanch@protonmail.com>
2020-12-30 08:39:11 +0100

    (#1) Add solution for part two of Day 6

--- 06-C11/main.c
+++ 06-C11/main.c
@@ -2,150 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define MAXLINEBUFFER 1024
 #define MAXGROUPS 1024
-
-/* char *intersect(char *src, char *dst, int line) { */
-
-/*     // remove return carriage */
-
-/*     if (line > 0) { */
-
-/*         // intersect any occurrence between two strings */
-/*         // i.e */
-/*         // ab */
-/*         // ac */
-/*         // -> */
-/*         // a */
-
-/*         // we need a occurrences buffer */
-/*         char *occurrences = malloc(sizeof(char) * MAXLINEBUFFER); */
-/*         occurrences[0] = '\0'; */
-
-/*         int match = 0; */
-
-/*         for (char *ptr_src = src; *ptr_src != '\0'; ptr_src++) { */
-/*             for (char *ptr_dst = dst; *ptr_dst != '\0'; ptr_dst++) { */
-
-/*                 if (*ptr_src == *ptr_dst) { */
-/*                     match = 1; */
-/*                     break; */
-/*                 } */
-
-/*                 if (*ptr_src != *ptr_dst) { */
-/*                     match = 0; */
-/*                 } */
-/*             } */
-
-/*             if (match) { */
-/*                 size_t len = strlen(occurrences); */
-/*                 char *buffer = malloc(sizeof(char) * len + 1 + 1); */
-/*                 strcpy(buffer, occurrences); */
-/*                 buffer[len] = *ptr_src; */
-/*                 buffer[len + 1] = '\0'; */
-/*                 strcpy(occurrences, buffer); */
-/*                 free(buffer); */
-/*                 match = 0; */
-/*             } */
-/*         } */
-
-/*         return occurrences; */
-
-/*     } else { */
-/*         // first of all, remove return carriage */
-/*         if (src[strlen(src) - 1] == '\r' || src[strlen(src) - 1] == '\n') */
-/*             src[strlen(src) - 1] = '\0'; */
-
-/*         // copy literally src to dst */
-/*         dst = malloc(sizeof(char) * strlen(src)); */
-/*         dst[0] = '\0'; */
-/*         // remove all the repeated occurrences */
-/*         // i.e: aaaaaabaaaacccaaabbb -> abc */
-/*         int match = 0; */
-/*         for (char *ptr_src = src; *ptr_src != '\0'; ptr_src++) { */
-
-/*             if (strlen(dst) == 0) { */
-/*                 size_t len = strlen(dst); */
-/*                 char *buffer = malloc(sizeof(char) * len + 1 + 1); */
-/*                 buffer[len] = *ptr_src; */
-/*                 buffer[len + 1] = '\0'; */
-/*                 strcpy(dst, buffer); */
-/*                 free(buffer); */
-/*             } */
-
-/*             for (char *ptr_dst = dst; *ptr_dst != '\0'; ptr_dst++) { */
-/*                 if (*ptr_src == *ptr_dst) { */
-/*                     match = 0; */
-/*                     break; */
-/*                 } */
-
-/*                 if (*ptr_src != *ptr_dst) { */
-/*                     match = 1; */
-/*                 } */
-/*             } */
-
-/*             if (match) { */
-/*                 size_t len = strlen(dst); */
-/*                 char *buffer = malloc(sizeof(char) * len + 1 + 1); */
-/*                 strcpy(buffer, dst); */
-/*                 buffer[len] = *ptr_src; */
-/*                 buffer[len + 1] = '\0'; */
-/*                 strcpy(dst, buffer); */
-/*                 free(buffer); */
-/*                 match = 0; */
-/*             } */
-/*         } */
-/*         /\* printf("src: %s\n", src); *\/ */
-/*         /\* printf("dst: %s\n", dst); *\/ */
-/*     } */
-
-/*     return dst; */
-/* } */
-
-/* int dumpGroupData(char** input) { */
-/*     char *yes_to_questions; */
-
-/*     int line =0; */
-/*     for (char **ptr = input; *ptr != NULL; ptr++) { */
-/*         yes_to_questions = intersect(*ptr, yes_to_questions, line); */
-/*         line++; */
-/*     } */
-/*     return (int) strlen(yes_to_questions); */
-/* } */
-
-/* int partTwo() { */
-/*     int total_yes_to_questions = 0; */
-/*     int group = 0; */
-/*     printf("Group :%d\n", group); */
-/*     group++; */
-/*     char** stackGroups = (char**)malloc(sizeof(char) * MAXGROUPS); */
-/*     int index = 0; */
-/*     for (int i = 0; i < g_inputFile.lines; i++) { */
-/*         if (g_inputFile.input[i][0] == '\n') { // check new line = group */
-/*             // set end of array of strings */
-/*             stackGroups[index] =NULL; */
-/*             // dump or processing data from stackGroups */
-/*             total_yes_to_questions += dumpGroupData(stackGroups); */
-/*             // reset index */
-/*             index = 0; */
-
-/*             /\* printf("Group :%d\n", group); *\/ */
-/*             group++; */
-/*         } else { */
-/*             stackGroups[index] = g_inputFile.input[i]; */
-/*             /\* printf(">%s", stackGroups[index]); *\/ */
-/*             index++; */
-/*         } */
-/*     } */
-/*     // matching last group */
-/*     stackGroups[index] = NULL; */
-/*     total_yes_to_questions += dumpGroupData(stackGroups); */
-/*     index = 0; */
-
-/*     return total_yes_to_questions; */
-/* } */
-
 
 int any_yesu(char **answers) {
     char* result = (char*)malloc(sizeof(char) * MAXLINEBUFFER);
@@ -169,7 +27,7 @@ int any_yesu(char **answers) {
                         match++;
                     }
                 }
-                if (!match) {
+                if (!match ) {
                     result[i] = *chr;
                     result[i + 1] = '\0';
                     i++;
@@ -184,19 +42,79 @@ int any_yesu(char **answers) {
 }
 
 int every_yesu(char **answers) {
+    char* result = (char*)malloc(sizeof(char) * MAXLINEBUFFER);
+    result[0] = '\0';
+
+    int i = 0;
+    int match = 0;
+    int people = 0;
+
+    // concatenate all the answers in one line
     for (char **answer = answers; *answer != NULL; answer++) {
-        /* printf("%s", *answer); */
+        for (char *chr = *answer; *chr != '\0'; chr++) {
+                result[i] = *chr;
+                result[i + 1] = '\0';
+                i++;
+        }
+        people++;
     }
 
-    return 0;
+    // simplest solution is almost always the best solution
+    int k = 0;
+    char* buffer = (char*)malloc(sizeof(char) * MAXLINEBUFFER);
+    buffer[0] = '\0';
+
+    for (int i = 0; i < (int)strlen(result); i++) {
+        for (int j = 0; j < (int)strlen(result); j++) {
+            if (result[i] == result[j]) {
+                match++;
+            }
+        }
+        if ((match / people) > 0) {
+            buffer[k] = result[i];
+            buffer[k + 1] = '\0';
+            k++;
+        }
+        match = 0;
+    }
+
+    // remove all repeated occurrences of same character
+    result[0] = '\0';
+    strcpy(result, buffer);
+    buffer[0] = '\0';
+    k = 0;
+    match = 0;
+    for (int i = 0; i < (int)strlen(result); i++) {
+        if (strlen(buffer) == 0) {
+            buffer[k] = result[i];
+            buffer[k + 1] = '\0';
+            k++;
+        } else {
+            for (int j = 0; j < (int)strlen(buffer); j++) {
+                if (result[i] == buffer[j]) {
+                    match++;
+                }
+            }
+            if (!match) {
+                buffer[k] = result[i];
+                buffer[k + 1] = '\0';
+                k++;
+            }
+            match = 0;
+        }
+    }
+
+    result = buffer;
+    int len = strlen(result);
+    return len;
 }
 
 void solve_file(char *filename)
 {
     FILE *fp;
     char** lines;
-    int nlines = 0;
     char** answers;
+    int nlines = 0;
     int any_result = 0;
     int every_result = 0;
 
@@ -249,6 +167,7 @@ void solve_file(char *filename)
     any_result += any_yesu(answers);
     every_result += every_yesu(answers);
 
+    printf("Puzzle input file: %s\n", filename);
     printf("Part 1: %d\n", any_result);
     printf("Part 2: %d\n", every_result);
 
