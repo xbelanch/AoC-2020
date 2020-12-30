ac1530398d1c43ae4f3149d9b136253b78fd184f
Xavier Belanche <xbelanch@protonmail.com>
2020-12-30 14:11:12 +0100

    [07]: Add reading lines and store them for processing later

--- 07-C11/main.c
+++ 07-C11/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_BUFFER_LINE 512
+
 int countlines(char *filename){
   int lines = 0;
-  FILE* fp = fopen(filename, "rb");
+  FILE* fp = fopen(filename, "r");
 
   while(!feof(fp)) {
     char ch = fgetc(fp);
@@ -15,11 +17,25 @@ int countlines(char *filename){
   return lines;
 }
 
-
 void solve_file(char* filename) {
-    printf("Filename: %s\n", filename);
-    int max = countlines(filename);
-    printf("Number of lines of %s file: %d\n", filename, max);
+
+  FILE *fp = fopen(filename, "r");
+  int max = countlines(filename);
+  char **lines = (char**)malloc(sizeof(char*) * max);
+
+  printf("Filename: %s\n", filename);
+  printf("Number of lines of %s file: %d\n", filename, max);
+
+  // store all the lines from input filename to lines var
+  for (int i = 0; i < max; i++) {
+    lines[i] = (char *)malloc(MAX_BUFFER_LINE);
+    if (fgets(lines[i], MAX_BUFFER_LINE + 1, fp) == NULL) {
+      printf("ERROR, line too long at %d\n", i);
+    } else {
+      printf("%d>%s", i, lines[i]);
+    }
+  }
+  fclose(fp);
 }
 
 int main(int argc, char *argv[])
