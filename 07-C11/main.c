112bb04c17e7e92a44abf4f84f2bf47301bb2c4c
Xavier Belanche <xbelanch@protonmail.com>
2020-12-30 12:58:12 +0100

    [07]: Add input puzzle file and poggering with dumb functions

--- 07-C11/main.c
+++ 07-C11/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int countlines(char *filename){
+  int lines = 0;
+  FILE* fp = fopen(filename, "rb");
+
+  while(!feof(fp)) {
+    char ch = fgetc(fp);
+    if(ch == '\n') {
+      lines++;
+    }
+  }
+  fclose(fp);
+  return lines;
+}
+
+
 void solve_file(char* filename) {
-    printf("Filename: %s", filename);
+    printf("Filename: %s\n", filename);
+    int max = countlines(filename);
+    printf("Number of lines of %s file: %d\n", filename, max);
 }
 
 int main(int argc, char *argv[])
