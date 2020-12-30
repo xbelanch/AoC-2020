5fcb6a95b6ab92f6e6711b0f03cb295d2bc1151d
Xavier Belanche <xbelanch@protonmail.com>
2020-12-30 19:50:37 +0100

    [07]: Add buffer to store all splitted lines from input puzzle files

--- 07-C11/main.c
+++ 07-C11/main.c
@@ -1,41 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_BUFFER_LINE 512
+#define MAX_BUFFER_SIZE 2048
 
 int countlines(char *filename){
-  int lines = 0;
-  FILE* fp = fopen(filename, "r");
+    int lines = 0;
+    FILE* fp = fopen(filename, "r");
 
-  while(!feof(fp)) {
-    char ch = fgetc(fp);
-    if(ch == '\n') {
-      lines++;
+    while(!feof(fp)) {
+        char ch = fgetc(fp);
+        if(ch == '\n') {
+            lines++;
+        }
     }
-  }
-  fclose(fp);
-  return lines;
+    fclose(fp);
+    return lines;
 }
 
 void solve_file(char* filename) {
 
-  FILE *fp = fopen(filename, "r");
-  int max = countlines(filename);
-  char **lines = (char**)malloc(sizeof(char*) * max);
+    FILE *fp = fopen(filename, "r");
+    int max = countlines(filename);
+    char **lines = (char**)malloc(sizeof(char*) * max);
+    char **buffer = (char**)malloc(sizeof(char*) * MAX_BUFFER_SIZE);
 
-  printf("Filename: %s\n", filename);
-  printf("Number of lines of %s file: %d\n", filename, max);
+    printf("Filename: %s\n", filename);
+    printf("Number of lines of %s file: %d\n", filename, max);
 
-  // store all the lines from input filename to lines var
-  for (int i = 0; i < max; i++) {
-    lines[i] = (char *)malloc(MAX_BUFFER_LINE);
-    if (fgets(lines[i], MAX_BUFFER_LINE + 1, fp) == NULL) {
-      printf("ERROR, line too long at %d\n", i);
-    } else {
-      printf("%d>%s", i, lines[i]);
+    // store all the lines from input filename to lines var
+    for (int i, j =  0; i < max; i++) {
+        lines[i] = (char *)malloc(MAX_BUFFER_LINE);
+        if (fgets(lines[i], MAX_BUFFER_LINE + 1, fp) == NULL) {
+            printf("ERROR, line too long at %d\n", i);
+        } else {
+            // parse every line, splitting string into tokens
+            // and store onto buffer
+            char *pch = strtok(lines[i], ",.\r\n");
+            int k = 0;
+            while (pch != NULL) {
+                buffer[j + k] = malloc(sizeof(char) * strlen(pch) + 1);
+                buffer[j + k][0] = '\0';
+                memcpy(buffer[j + k], pch, strlen(pch));
+                buffer[j + k][strlen(pch)] = '\0';
+                pch = strtok(NULL, ",.\r\n");
+                k++;
+            }
+            j += k;
+            buffer[j + 1] = NULL;
+        }
     }
-  }
-  fclose(fp);
+
+    // print buffer to stdout
+    int i = 0;
+    for (char** ptr = buffer; *ptr != NULL; ptr++) {
+        printf("buffer[%d]: %s\n", i, *ptr);
+        i++;
+    }
+
+    free(lines);
+    free(buffer);
+    fclose(fp);
 }
 
 int main(int argc, char *argv[])
