a829c0482366798972eb8dfb2240fdeccd1f96a8
Xavier Belanche <xbelanch@protonmail.com>
2021-01-09 13:19:21 +0100

    (#6) Refactored solution code of part one

--- 07-C11/main.c
+++ 07-C11/main.c
@@ -3,127 +3,235 @@
 #include <string.h>
 #include "yastring.h"
 
-#define MAXCHILDRENSIZE 32
-#define MAXSIZELINE 512
-#define MAXSIZERULES 1024
-#define MAXSIZECOLORS 1024
+#define MAXSIZELINES 2048
 
 typedef struct child {
-    char *name;
+    char *color;
     int amount;
-    struct rule *rule;
+    struct child *next;
 } Child;
 
 typedef struct rule {
-    char *key;
-    Child *children[MAXCHILDRENSIZE];
+    char *color;
+    struct child *contain;
+    struct rule *next;
 } Rule;
 
-// Global variables
-Rule *rules[MAXSIZERULES];
-char *colors[MAXSIZECOLORS];
-int ncolors;
+typedef struct container {
+    char *color;
+    int amount;
+    char *rule;
+    struct container *next;
+} Container;
+
+typedef struct visited {
+    char *color;
+    struct visited *next;
+} Visited;
+
+Rule *createNewRule(char *color) {
+    Rule *rule = (Rule*)malloc(sizeof(Rule));
+    if (rule == NULL)
+        return NULL;
+
+    rule->color = color;
+    rule->contain = NULL;
+    return rule;
+}
+
+int addRuleToList(Rule **rules, Rule *rule) {
+    if (*rules == NULL) {
+        *rules = rule;
+        rule->next = NULL;
+    }
+    else {
+        rule->next = *rules;
+        *rules = rule;
+    }
+    return 0;
+}
+
+Child *createNewChild(char *color, int amount) {
+    Child *child = (Child*)malloc(sizeof(Child));
+    if (child == NULL)
+        return NULL;
+
+    child->amount = amount;
+    child->color = color;
+
+    return child;
+}
+
+int addChildToRule(Rule *rule, Child *child) {
+    if (rule->contain == NULL) {
+        rule->contain = child;
+        rule->contain->next = NULL;
+    } else {
+        child->next = rule->contain;
+        rule->contain = child;
+    }
+
+    return 0;
+}
+
+size_t printRuleContainer(Rule *rule) {
+    Child *ptr = rule->contain;
+    size_t length = 0;
+    while (ptr != NULL) {
+        printf("%d of %s\n", ptr->amount, ptr->color);
+        ptr = ptr->next;
+        length++;
+    }
+    return length;
+}
+
+size_t lengthListOfRules(Rule *rules) {
+    Rule *ptr = rules;
+    size_t length = 0;
+    while (ptr != NULL){
+        ptr = ptr->next;
+        length++;
+    }
+    return length;
+}
+
+void printListOfRules(Rule *rules) {
+    Rule *ptr = rules;
+    while (ptr != NULL) {
+        printf(">>%s\n", ptr->color);
+        int container = printRuleContainer(ptr);
+        if (container == 0)
+            printf("contain no bags\n");
+        ptr = ptr->next;
+    }
+}
+
+// given a rules and color for matching
+// return the name of rules of colors that have almost one of that color
+void findContainers(Rule *rules, char *color, Container **container) {
+
+    for (Rule *rl_ptr = rules; rl_ptr != NULL; rl_ptr = rl_ptr->next) {
+        Child *chld_ptr = rl_ptr->contain;
+        if (chld_ptr != NULL) { // avoid rules of bags thats doesnt have any bag color
+            for (; chld_ptr != NULL; chld_ptr = chld_ptr->next) {
+                if (strcmp(chld_ptr->color, color) == 0) { // !match
+
+
+                    if (container == NULL) {
+                        Container *container = (Container*) malloc(sizeof(Container));
+                        container->color = chld_ptr->color;
+                        container->amount = chld_ptr->amount;
+                        container->rule = rl_ptr->color;
+                        container->next = NULL;
+                    } else {
+                        Container *new = (Container*) malloc(sizeof(Container));
+                        new->color = chld_ptr->color;
+                        new->amount = chld_ptr->amount;
+                        new->rule = rl_ptr->color;
+                        new->next = *container;
+                        *container = new;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void countBags(Rule *rules, char *color, Visited **visited) {
+    int found = 0;
+
+    if (*visited == NULL) {
+        Visited *new = (Visited*) malloc(sizeof(Visited));
+        new->color = color;
+        new->next = *visited;
+        *visited = new;
+        found = 1;
+    } else {
+        for (Visited *vptr = *visited; vptr != NULL; vptr = vptr->next) {
+            if (strcmp(vptr->color, color) == 0) {
+                found++;
+            }
+        }
+    }
+
+    if (found == 0) {
+        Visited *new = (Visited*) malloc(sizeof(Visited));
+        new->color = color;
+        new->next = *visited;
+        *visited = new;
+    }
+
+    // That's so important! reset container for every iteration!
+    Container *container = NULL;
+    findContainers(rules, color, &container);
+
+    // Recursion is coming!
+    for (Container *ptr = container; ptr != NULL; ptr = ptr->next) {
+        countBags(rules, ptr->rule, visited);
+    }
+}
 
 Rule *lineToRule(char *line) {
+    Rule *rule = NULL;
+    Child *child = NULL;
 
-    // create a blank rule
-    Rule *rule = (Rule*)malloc(sizeof(Rule));
-
-    size_t len = 0;
-
-    // discard carriage return or new line
+    // Guess there's another smart way to do next
+    // remove carriage return or new line
     strtok(line, "\r");
     strtok(line, "\n");
 
     // split input line by delimiter "contain "
-    // in two strings: rule and container
+    // in two strings: bag color and container
     char *delimiter = "contain ";
-    char *pch = strstr(line, delimiter);
-    pch += strlen(delimiter);
+    char *contain = (char*) malloc(sizeof(char) * strlen(line));
+    contain = strstr(line, delimiter);
+    contain += strlen(delimiter);
+    contain[strlen(contain) - 1] = '\0';
 
-    // get container
-    char *container = malloc(sizeof(char) * strlen(pch) + 1);
-    strncpy(container, pch, strlen(pch));
-    container[strlen(container) - 1] = '\0'; // remove last dot
-
-    // get key rule
-    char *key = malloc(sizeof(char) * strlen(line) + 1);
-    len = strlen(line) - strlen(pch) - strlen(delimiter);
-    strncpy(key, line, len);
-    key[len - 1] = '\0'; // remove last space
+    size_t len = strlen(line) - strlen(contain) - strlen(delimiter) - 1; // minus one for extra space
+    char *bag = (char*) malloc(sizeof(char) * len);
+    strncpy(bag, line, len);
+    bag[len] = '\0';
 
     // trim bag word from rule and container
-    strremove(key, " bags");
-    strremove(key, " bag");
-    strremove(container, " bags");
-    strremove(container, " bag");
+    strremove(bag, " bags");
+    strremove(bag, " bag");
+    strremove(contain, " bags");
+    strremove(contain, " bag");
 
-    // assign key to rule
-    rule->key = key;
+    rule = createNewRule(bag);
 
-    // split container in several entries or bags
-    char *bag = malloc(sizeof(char) * MAXSIZELINE);
-    int nchild = 0;
-    while ((bag = strsplit(&container, ",")) != NULL) {
-        if (bag[0] == ' ') {
-            bag++;
-        }
+    char *color = (char*) malloc(sizeof(char) * 128);
+    while ((color = strsplit(&contain, ",")) != NULL) {
+        if (color[0] == ' ') color++;
 
-        if (strcmp(bag, "no other") == 0) {
-            rule->children[0] = NULL;
+        if (strcmp(color, "no other") == 0) {
+            rule->contain = NULL;
             return rule;
         } else {
-            // create a children
-            Child *child = (Child*)malloc(sizeof(Child));
-            child->rule = rule;
 
             // get amount of bags
-            len = strlen(bag) - strlen(strstr(bag, " "));
-            char *number = malloc(sizeof(char) * MAXSIZELINE);
-            strncpy(number, bag, len);
+            // this is a weird manner of extract that number
+            // but...
+            len = strlen(color) - strlen(strstr(color, " "));
+            char *number = malloc(sizeof(char) * 3);
+            strncpy(number, color, len);
             number[len] = '\0';
             int amount = atoi(number);
-            child->amount = amount;
-
-            // get name of bags
-            char *pstr = strstr(bag, " ");
-            pstr++;
-
-            // check if entry is the last one
-            // @TODO: I don't know why last entry adds some extra characters at the end of the string
-            char *dot = strstr(pstr, ".");
-            if (dot != NULL) {
-                len = strlen(pstr) - strlen(dot);
-                char *color = malloc(sizeof(char) * MAXSIZELINE);
-                strncpy(color, pstr, len);
-                color[len] = '\0';
-                child->name = color;
-            } else {
-                child->name = pstr;
-            }
-
-            // Add child to rule
-            rule->children[nchild] = child;
-            rule->children[nchild + 1] = NULL;
-            nchild++;
+            // ... is what I actually know how to do that shit!
+            color += strlen(number) + 1;
+            len = strlen(color) - strlen(number) + 1;
+            char *childname = malloc(sizeof(char) * len);
+            strncpy(childname, color, len);
+            childname[len] = '\0';
+            child = createNewChild(childname, amount);
+            addChildToRule(rule, child);
         }
     }
-
-    free(bag);
     return rule;
 }
 
-void printPrettyRules(Rule *rule) {
-    printf("Rule> %s:\n", rule->key);
-    if (rule->children[0] != 0) {
-        for (int i = 0; rule->children[i] != NULL; i++)
-            printf("  %d of %s\n", rule->children[i]->amount, rule->children[i]->name);
-    } else {
-        printf( "  contain no other bags\n");
-    }
-}
-
-void rulesFromFile(char *filepath) {
+void createRulesFromFile(Rule **rules, char *filepath) {
     FILE *fp = fopen(filepath, "r");
 
     if (fp == NULL) {
@@ -131,113 +239,41 @@ void rulesFromFile(char *filepath) {
         exit(0);
     }
 
-    char lines[MAXSIZELINE];
-    int nrules = 0;
+    char lines[MAXSIZELINES];
     while (fgets(lines, sizeof(lines), fp) != NULL) {
-        rules[nrules] = lineToRule(lines);
-        rules[nrules + 1] = NULL;
-        nrules++;
+        Rule *rule = lineToRule(lines);
+        addRuleToList(rules, rule);
     }
 
     fclose(fp);
- }
-
-void printGraphvizOfRules() {
-    printf("digraph Rules {\n");
-    for (int i = 0 ; rules[i] != NULL; i++) {
-        if (rules[i]->children[0] != NULL) {
-            for (int j = 0; rules[i]->children[j] != NULL; j++) {
-                printf("\t\"%s\" -> \"%s\";\n", rules[i]->key, rules[i]->children[j]->name);
-            }
-        } else {
-            printf("\t\"%s\" -> \"no bags\";\n", rules[i]->key);
-        }
-    }
-    printf("}\n");
 }
 
-Child **findContainers(char *color) {
-    Child **containers = (Child**)malloc(sizeof(Child) * 512);
-    int count = 0;
-    containers[count] = NULL;
+int solveFilePartOne(char *filepath) {
+    Rule *rules = NULL;
+    createRulesFromFile(&rules, filepath);
 
-    for (int i = 0 ; rules[i] != NULL; i++) {
-        if (rules[i]->children[0] != NULL) {
-            for (int j = 0; rules[i]->children[j] != NULL; j++) {
-                if (strcmp(rules[i]->children[j]->name, color) == 0) {
-                    containers[count] = *(rules[i]->children);
-                    containers[count + 1] = NULL;
-                    count++;
-                }
-            }
-        }
-    }
+    /* printListOfRules(rules); */
+    printf("Length of rules: %lld\n", lengthListOfRules(rules));
 
-    return containers;
-}
+    Visited *visited = NULL;
+    countBags(rules, "shiny gold", &visited);
 
-void countWays (char *color) {
-    colors[ncolors] = color;
-    colors[ncolors + 1] = NULL;
-    ncolors++;
-
-    Child **containers = findContainers(color);
-
+    // count visited colors
     int len = 0;
-    while (containers[len] != NULL) {
+    for (Visited *ptr = visited; ptr != NULL; ptr = ptr->next) {
         len++;
     }
-    if (len > 0) {
-        for (int i = 0; i < len; i++) {
-            countWays(containers[i]->rule->key);
-        }
-    }
+
+    return len - 1; // minus one because input color
 }
-
-
-int solveFile(char *filepath) {
-    rulesFromFile(filepath);
-    /* printGraphvizOfRules(); */
-    char *start = "shiny gold";
-    ncolors = 0;
-    colors[ncolors] = NULL;
-
-    // recursive function to count ways from start color to rule color
-    countWays(start);
-
-    // how many colors has been collected?
-    int lencolors = 0;
-    while (colors[lencolors] != NULL) {
-        lencolors++;
-    }
-
-
-    // remove duplicate colors
-    int count = 0;
-    char* tmp[MAXSIZECOLORS];
-    int d;
-    for (int c = 0; c < lencolors; c++) {
-        for (d = 0; d < count; d++) {
-            if(strcmp(colors[c], tmp[d]) == 0)
-                break;
-        }
-        if (d == count) {
-            tmp[count] = colors[c];
-            count++;
-        }
-    }
-
-    // remove one because star color not count
-    return count - 1;
-}
-
 
 int main(int argc, char *argv[])
 {
     for (int i = 1; i < argc; i++) {
         printf("Input puzzle: %s\n", argv[i]);
-        int partOne = solveFile(argv[i]);
+        int partOne = solveFilePartOne(argv[i]);
         printf("Part one solution: %d\n", partOne);
+
     }
     return 0;
 }
