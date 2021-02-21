78cb0a54ba3491efed614bd67e77bd664c71ef02
Xavier Belanche <xbelanch@protonmail.com>
2021-02-21 16:52:38 +0100

    [11]: Added simple and dummy rasycast routine for "first seat they can see" in each of eight directions

--- 11-C11/main.c
+++ 11-C11/main.c
@@ -21,6 +21,169 @@ State checkSeatPosition(char **layout, int row, int column) {
     return isUnknown;
 }
 
+int* raycast(char **layout, int size, int row, int column) {
+
+    // 0 -> occupied
+    // 1 -> empty
+    int* seats = malloc(sizeof(int) * 2);
+    seats[0] = 0;
+    seats[1] = 0;
+
+    int width = (int) (strlen(layout[0]) - 1);
+
+    for (int i = row - 1; i <= row + 1; i++) {
+        if (i >= 0 && i <= size ) {
+            for (int j = column - 1; j <= column + 1; j++) {
+                if (j >= 0 && j < width) {
+                    if (layout[i][j] == '.') {
+                        // cast a ray until reaching limit of canvas seat
+                        int dirX = j - column;
+                        int dirY = i - row;
+                        // Right
+                        if (dirX == 1 && dirY == 0) {
+                            for (int steps = 0; steps < (width - column - 1); steps++) {
+                                if (layout[i][j + steps] != '#' && layout[i][j + steps] != 'L') {
+                                    /* layout[i][j + steps] = '*'; */
+                                }
+                                if (layout[i][j + steps] == '#') {
+                                    seats[0]++;
+                                    break;
+                                }
+                                if (layout[i][j + steps] == 'L') {
+                                    seats[1]++;
+                                    break;
+                                }
+                            }
+                        }
+                        // Left
+                        if (dirX == -1 && dirY == 0) {
+                            for (int steps = 0;  (column - steps) >= 0; steps++) {
+                                if (layout[i][j - steps] != '#' && layout[i][j - steps] != 'L') {
+                                    /* layout[i][j - steps] = '*'; */
+                                }
+                                if (layout[i][j - steps] == '#') {
+                                    seats[0]++;
+                                    break;
+                                }
+                                if (layout[i][j - steps] == 'L') {
+                                    seats[1]++;
+                                    break;
+                                }
+                            }
+                        }
+                        // Down
+                        if (dirX == 0 && dirY == 1) {
+                            for (int steps = 0; steps < (size - row - 1); steps++) {
+                                if (layout[i + steps][j] != '#' && layout[i + steps][j] != 'L') {
+                                    /* layout[i + steps][j] = '*'; */
+                                }
+                                if (layout[i + steps][j] == '#') {
+                                    seats[0]++;
+                                    break;
+                                }
+                                if (layout[i + steps][j] == 'L') {
+                                    seats[1]++;
+                                    break;
+                                }
+                            }
+                        }
+
+                        // Up
+                        if (dirX == 0 && dirY == -1) {
+                            for (int steps = 0; (row - steps - 1) >= 0; steps++) {
+                                if (layout[i - steps][j] != '#' && layout[i - steps][j] != 'L') {
+                                    /* layout[i - steps][j] = '*'; */
+                                }
+                                if (layout[i - steps][j] == '#') {
+                                    seats[0]++;
+                                    break;
+                                }
+                                if (layout[i - steps][j] == 'L') {
+                                    seats[1]++;
+                                    break;
+                                }
+                            }
+                        }
+
+                        // Right - Up
+                        if ((dirX == 1) && (dirY == -1)) {
+                            for (int stepsX = 0, stepsY = 0 ; stepsX < (width - column) && (row - stepsY - 1 >= 0); stepsX++, stepsY++) {
+                                if (layout[i - stepsY][j + stepsX] != '#' && layout[i - stepsY][j + stepsX] != 'L') {
+                                    /* layout[i - stepsY][j + stepsX] = '*'; */
+                                }
+                                if (layout[i - stepsY][j + stepsX] == '#') {
+                                    seats[0]++;
+                                    break;
+                                }
+                                if (layout[i - stepsY][j + stepsX] == 'L') {
+                                    seats[1]++;
+                                    break;
+                                }
+                            }
+                        }
+
+                        // Right - Down
+                        if ((dirX == 1) && (dirY == 1)) {
+                            for (int stepsX = 0, stepsY = 0 ; stepsX < (width - column) && (row + stepsY + 1 < size); stepsX++, stepsY++) {
+                                if (layout[i + stepsY][j + stepsX] != '#' && layout[i + stepsY][j + stepsX] != 'L') {
+                                    /* layout[i + stepsY][j + stepsX] = '*'; */
+                                }
+                                if (layout[i + stepsY][j + stepsX] == '#') {
+                                    seats[0]++;
+                                    break;
+                                }
+                                if (layout[i + stepsY][j + stepsX] == 'L') {
+                                    seats[1]++;
+                                    break;
+                                }
+                            }
+                        }
+                        // Left - Down
+                        if ((dirX == -1) && (dirY == 1)) {
+                            for (int stepsX = 0, stepsY = 0 ; (width - stepsX) >= 0 && (row + stepsY + 1 < size); stepsX++, stepsY++) {
+                                if (layout[i + stepsY][j - stepsX] != '#' && layout[i + stepsY][j - stepsX] != 'L') {
+                                    /* layout[i + stepsY][j - stepsX] = '*'; */
+                                }
+                                if (layout[i + stepsY][j - stepsX] == '#') {
+                                    seats[0]++;
+                                    break;
+                                }
+                                if (layout[i + stepsY][j - stepsX] == 'L') {
+                                    seats[1]++;
+                                    break;
+                                }
+                            }
+                        }
+                        // Left - Up
+                        if ((dirX == -1) && (dirY == -1)) {
+                            for (int stepsX = 0, stepsY = 0 ; (width - stepsX) >= 0 && (row - stepsY - 1 >= 0); stepsX++, stepsY++) {
+                                if (layout[i - stepsY][j - stepsX] != '#' && layout[i - stepsY][j - stepsX] != 'L' ) {
+                                    /* layout[i - stepsY][j - stepsX] = '*'; */
+                                }
+                                if (layout[i - stepsY][j - stepsX] == '#') {
+                                    seats[0]++;
+                                    break;
+                                }
+                                if (layout[i - stepsY][j - stepsX] == 'L') {
+                                    seats[1]++;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if (layout[i][j] == '#')
+                        seats[0]++;
+
+                    if (layout[i][j] == 'L' && i != row && j != column)
+                        seats[1]++;
+                }
+            }
+        }
+    }
+    return seats;
+}
+
 int checkRuleOne(char **layout, int size, int row, int column) {
     // * - * - *
     // |   |   |
@@ -124,17 +287,20 @@ void solve_file(char *filename) {
 
     fclose(f);
 
-    int cycles = 0;
-    int run = 1;
-    while (run) {
-        if (runOneCycle(arr, arr_size) == 0)
-            run = 0;
-        cycles++;
-    }
+    /* int cycles = 0; */
+    /* int run = 1; */
+    /* while (run) { */
+    /*     if (runOneCycle(arr, arr_size) == 0) */
+    /*         run = 0; */
+    /*     cycles++; */
+    /* } */
 
-    printf("Cycles: %d\n", cycles);
-    printf("How many seats end up occupied? %d\n", countOccupiedSeats(arr, arr_size));
-
+    /* printf("Cycles: %d\n", cycles); */
+    /* printf("How many seats end up occupied? %d\n", countOccupiedSeats(arr, arr_size)); */
+    int* t = raycast(arr, arr_size, 4, 3);
+    printSeatLayout(arr, arr_size);
+    printf("Occupied: %d\n", t[0]);
+    printf("Empty: %d\n", t[1]);
 }
 
 int main(int argc, char *argv[])
