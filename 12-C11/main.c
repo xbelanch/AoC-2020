53845ff8080dbe7eaf5ba1f9abf051d379783132
Xavier Belanche <xbelanch@protonmail.com>
2021-03-06 11:51:34 +0100

    [12] Parse and store values from input file

--- 12-C11/main.c
+++ 12-C11/main.c
@@ -1,9 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+/* #include <SDL2/SDL.h> */
 
-int solve_file(char *filename) {
+#define MAXINSTRUCTIONSSIZE 2048
+
+typedef struct _instruction {
+    char action;
+    int value;
+} Instruction;
+
+int solve_file(const char *filename) {
     printf("Input file: %s\n", filename);
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) {
+        perror("File opening failed\n");
+        return EXIT_FAILURE;
+    }
+    Instruction instructions[MAXINSTRUCTIONSSIZE];
+    int size = 0;
+
+    while (!feof(fp)) {
+        instructions[size] = (Instruction){0};
+        fscanf(fp, "%1c%d\r\n", &instructions[size].action, &instructions[size].value);
+        size += 1;
+    }
+
+    // Print parsed input file
+    for (int i = 0; i < size; ++i) {
+        printf("Get %c:%d\n", instructions[i].action, instructions[i].value);
+    }
+
+    fclose(fp);
     return 0;
 }
 
