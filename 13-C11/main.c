a4ab3b9447cfad677e066e30fd1386a76014f447
Xavier Belanche <xbelanch@protonmail.com>
2021-03-13 12:10:15 +0100

    [13]: Open, Read and print to stdout input files

--- 13-C11/main.c
+++ 13-C11/main.c
@@ -3,9 +3,24 @@
 
 int solve_file(char *filename) {
     printf("Input file: %s\n", filename);
+    FILE *fp = fopen(filename, "r");
+
+    if (!fp) {
+        perror("File opening failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // read input file content
+    const size_t line_size = 300;
+    char *line = (char*)malloc(sizeof(char) * line_size);
+    while (fgets(line, 300, fp) != NULL) {
+        printf(line);
+    }
+
+    free(line);
+    fclose(fp);
     return 0;
 }
-
 
 int main(int argc, char *argv[])
 {
