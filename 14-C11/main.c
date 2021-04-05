a3268aaba2a3a72dc8ad6a0fb8ed3b5534ca89f7
Xavier Belanche <xbelanch@protonmail.com>
2021-04-05 14:20:09 +0200

    [14]: Store input files as lines to parse them later

--- 14-C11/main.c
+++ 14-C11/main.c
@@ -1,8 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE_LINES 2048
+#define MAX_SIZE_CHARACTERS 512
+
+int todo(){
+    printf("TODO:\n");
+    return 0;
+}
 
 int solve_file(char *filepath) {
     printf("%s\n", filepath);
+    FILE *fp = fopen(filepath, "r");
+    char chr;
+    char buffer[MAX_SIZE_CHARACTERS];
+    char lines[MAX_SIZE_LINES][MAX_SIZE_CHARACTERS];
+    char *ptr = buffer;
+    int num = 0;
+
+    // read and store the input file
+    while (1) {
+        chr = fgetc(fp);
+        if (feof(fp))
+            break;
+        if (chr == '\n') {
+            *ptr = '\0';
+            strcpy(lines[num++], buffer);
+            ptr = buffer;
+
+        } else {
+            *ptr++ = chr;
+        }
+    }
+
+    // print to stdout
+    for (int i = 0; i < num; i++) {
+        fprintf(stdout, "line[%d]: %s\n", i, lines[i]);
+    }
+
+    fclose(fp);
     return 0;
 }
 
