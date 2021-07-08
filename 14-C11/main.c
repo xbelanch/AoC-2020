5eaa27e20a5831427bd038a2f20621abcd33c48e
Xavier Belanche <xbelanch@protonmail.com>
2021-07-08 14:45:15 +0200

    [14] Rewrite some code before to start implementing a parser

--- 14-C11/main.c
+++ 14-C11/main.c
@@ -6,10 +6,16 @@
 
 typedef struct line {
     char* text;
-    unsigned int number;
 } Line;
 
-Line aline = {0};
+Line lines[MAX_SIZE_LINES];
+
+int parse_input_file(int size_lines) {
+    for (int i = 0; i < size_lines; ++i) {
+            fprintf(stdout, "%u: %s\n", i, lines[i].text);
+    }
+    return 0;
+}
 
 int input_file(char *filename){
     int error = -1;
@@ -24,21 +30,22 @@ int input_file(char *filename){
     int c = fgetc(input_file);
     char* string = (char *)malloc(sizeof(char) * 512);
     size_t i = 0;
-    unsigned int nline = 0;
+    int nline = 0;
+
     while (EOF != c) {
         string[i++] = c;
         if ('\n' == c) {
             string[i-1] = '\0';
             size_t len = strlen(string);
-            aline.text = (char *)malloc(sizeof(char) * len);
-            memcpy(aline.text, string, len);
-            aline.number = nline;
-            fprintf(stdout, "%u: %s\n", aline.number, aline.text);
+            lines[nline].text = (char *)malloc(sizeof(char) * len);
+            memcpy(lines[nline].text, string, len);
             i = 0; nline++;
             string[i] = '\0';
         }
         c = fgetc(input_file);
     }
+
+    parse_input_file(nline);
 
     int success = fclose(input_file);
     return success;
