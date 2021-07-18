c71bbc179e08416a50437b84a36a96b28c5b033e
Xavier Belanche <xbelanch@protonmail.com>
2021-07-18 21:00:25 +0200

    [14] Add @tidwall hashmap implementation in C

---
+++ 14-C11/hashmap_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hashmap.h"
+
+struct user {
+    char *name;
+    int age;
+};
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv[0];
+    struct hashmap *map = hashmap_new(sizeof(struct user), 0, 0, 0, NULL, NULL, NULL);
+    hashmap_free(map);
+    return 0;
+}
