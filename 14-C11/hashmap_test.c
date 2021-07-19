159d54a0fefeb1dc898b5ef3c793920680436811
Xavier Belanche <xbelanch@protonmail.com>
2021-07-19 13:28:43 +0200

    Minor stupid commit

--- 14-C11/hashmap_test.c
+++ 14-C11/hashmap_test.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "hashmap.h"
+#include <limits.h>
+#include "./hashmap.c/hashmap.h"
 
-struct user {
-    char *name;
-    int age;
-};
+typedef struct memory {
+    size_t addr;
+    size_t value;
+} Memory;
 
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv[0];
-    struct hashmap *map = hashmap_new(sizeof(struct user), 0, 0, 0, NULL, NULL, NULL);
+    struct hashmap *map = hashmap_new(sizeof(Memory), USHRT_MAX, 0, 0, NULL, NULL, NULL);
     hashmap_free(map);
     return 0;
 }
