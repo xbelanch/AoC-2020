7b3b980b8e4e6e5d0976ea9ed4cea3bdf9717188
Xavier Belanche <xbelanch@protonmail.com>
2021-07-19 14:00:48 +0200

    [14]: Working on hashmap solution for part Two

--- 14-C11/hashmap_test.c
+++ 14-C11/hashmap_test.c
@@ -8,11 +8,27 @@ typedef struct memory {
     size_t value;
 } Memory;
 
+int value_compare(const void *a, const void *b, void *udata) {
+    return 0;
+}
+
+size_t memory_hash(const void *item, uint64_t seed0, uint64_t seed1) {
+    return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv[0];
-    struct hashmap *map = hashmap_new(sizeof(Memory), USHRT_MAX, 0, 0, NULL, NULL, NULL);
+    struct hashmap *map = hashmap_new(sizeof(Memory), 0, 0, 0, memory_hash, value_compare, NULL);
+
+    hashmap_set(map, &(Memory){ .addr = 0, .value = 15 });
+
+    Memory *mem;
+    mem = hashmap_get(map, &(Memory){ .addr = 0});
+    printf("Memory value: %lu at addr: %lu\n", mem->value, mem->addr);
+
     hashmap_free(map);
     return 0;
 }
