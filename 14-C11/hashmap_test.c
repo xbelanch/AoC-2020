ccd811884ea0d82f81b57df92280ab80faca71f8
Xavier Belanche <xbelanch@protonmail.com>
2021-07-20 12:16:39 +0200

    [14] Add basic setget hashmap operation from the example

--- 14-C11/hashmap_test.c
+++ 14-C11/hashmap_test.c
@@ -3,13 +3,15 @@
 #include <limits.h>
 #include "./hashmap.c/hashmap.h"
 
-typedef struct memory {
+struct memory {
     size_t addr;
     size_t value;
-} Memory;
+};
 
-int value_compare(const void *a, const void *b, void *udata) {
-    return 0;
+int address_compare(const void *a, const void *b, void *udata) {
+    const struct memory *memorya = a;
+    const struct memory *memoryb = b;
+    return memorya->addr > memoryb->addr ? 1 : 0;
 }
 
 size_t memory_hash(const void *item, uint64_t seed0, uint64_t seed1) {
@@ -21,12 +23,17 @@ int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv[0];
-    struct hashmap *map = hashmap_new(sizeof(Memory), 0, 0, 0, memory_hash, value_compare, NULL);
+    struct hashmap *map = hashmap_new(sizeof(struct memory), 0, 0, 0, memory_hash, address_compare, NULL);
 
-    hashmap_set(map, &(Memory){ .addr = 0, .value = 15 });
+    //    hashmap_set(map, &(struct memory){ .addr = (1UL << 35), .value = 25 });
+    hashmap_set(map, &(struct memory){ .addr = 198299, .value = 10 });
+    hashmap_set(map, &(struct memory){ .addr = 22927282, .value = 15 });
+    hashmap_set(map, &(struct memory){ .addr = 6787826627, .value = 20 });
 
-    Memory *mem;
-    mem = hashmap_get(map, &(Memory){ .addr = 0});
+    struct memory *mem;
+    mem = hashmap_get(map, &(struct memory){ .addr = 198299});
+    printf("Memory value: %lu at addr: %lu\n", mem->value, mem->addr);
+    mem->value = 32003;
     printf("Memory value: %lu at addr: %lu\n", mem->value, mem->addr);
 
     hashmap_free(map);
