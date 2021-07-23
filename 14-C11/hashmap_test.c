64c3275c8c1a3300082c11e9c87a124967b5295d
Xavier Belanche <xbelanch@protonmail.com>
2021-07-24 01:19:38 +0200

    [14] Add hashmap solution for part One of Day 14

--- 14-C11/hashmap_test.c
+++ 14-C11/hashmap_test.c
@@ -6,15 +6,24 @@
 #include "jelp.h"
 
 Line *lines;
+size_t result = 0;
 
 int address_compare(const void *a, const void *b, void *udata) {
     const Memory *_a = a;
     const Memory *_b = b;
-    return _a->addr > _b->addr ? 1 : 0;
+    return _a->addr < _b->addr ? 1 : 0;
 }
 
 size_t memory_hash(const void *item, uint64_t seed0, uint64_t seed1) {
-    return 0;
+    const Memory *memory = item;
+    return hashmap_sip(memory, sizeof(memory), seed0, seed1);
+}
+
+bool addr_iter(const void *item, void *udata) {
+    const Memory *memory = item;
+    // fprintf(stdout, "addr[%lu]: %lu\n", memory->addr, memory->value);
+    result += memory->value;
+    return true;
 }
 
 int main(int argc, char *argv[])
@@ -24,7 +33,7 @@ int main(int argc, char *argv[])
 
     struct hashmap *map = hashmap_new(sizeof(Memory), 0, 0, 0, memory_hash, address_compare, NULL);
 
-    char *sample_file = "sample-01.txt";
+    char *sample_file = "input.txt";
     FILE *input_file = fopen(sample_file, "rb");
     if (NULL == input_file) {
         fprintf(stderr, "ERROR: File \033[0;31m%s\033[0m not found.\n", sample_file);
@@ -52,6 +61,7 @@ int main(int argc, char *argv[])
 
     i = 0;
     size_t addr; size_t val;
+    char *mask;
     while (lines[i].string) {
         if (0 == strncmp(lines[i].string, "mem", 3)) {
             char *p_open_bracket = strchr(lines[i].string, '[');
@@ -70,7 +80,19 @@ int main(int argc, char *argv[])
             memcpy(value, ptr, len);
             val = atoll(value);
 
-            hashmap_set(map, &(Memory){ .addr = addr, .value = val });
+            // fprintf(stdout, "mem[%lu] = %lu\n", addr, val);
+            // apply mask to value
+            char *value_bitmasked = bitmask_op(mask, bin(val));
+            hashmap_set(map, &(Memory){
+                    .addr = addr,
+                    .value = bin2dec(value_bitmasked)
+                });
+        } else {
+            size_t len = strlen(lines[i].string) - 7;
+            mask = (char *)malloc(sizeof(char) * len);
+            char *ptr = lines[i].string + 7;
+            memcpy(mask, ptr, len);
+            // fprintf(stdout, "mask = %s\n", mask);
         }
         i++;
     }
@@ -78,10 +100,9 @@ int main(int argc, char *argv[])
     fclose(input_file);
 
     Memory *mem;
-    mem = hashmap_get(map, &(Memory){ .addr = 7});
-    fprintf(stdout, "Memory value: %lu at addr: %lu\n", mem->value, mem->addr);
-    mem = hashmap_get(map, &(Memory){ .addr = 8});
-    fprintf(stdout, "Memory value: %lu at addr: %lu\n", mem->value, mem->addr);
+    // Lets iterate over all memory addresses!
+    hashmap_scan(map, addr_iter, NULL);
+    fprintf(stdout, "Solution part One: %lu\n", result);
 
     hashmap_free(map);
     return 0;
