97878b50fe67735a259a83bb206430a26ca0089e
Xavier Belanche <xbelanch@protonmail.com>
2021-08-03 13:00:35 +0200

    [15] Read comma separated values and store it in an array

--- 15-C11/main.c
+++ 15-C11/main.c
@@ -1,12 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv[0];
 
+    int *sample = malloc(sizeof(int) * 32);
+
     char *input_sample_puzzle = "0,3,6";
+    char *ptr = input_sample_puzzle;
+    size_t size = 0;
+    while (*ptr != '\0') {
+        if (*ptr != ',')
+            sample[size++] = atoi(ptr);
+        ptr++;
+    }
+
+    for (size_t i = 0; i < size; ++i) {
+        fprintf(stdout, "data[%lu]: %d\n", i, sample[i]);
+    }
 
     fprintf(stdout, "Sample puzzle: %s\n", input_sample_puzzle);
     return 0;
