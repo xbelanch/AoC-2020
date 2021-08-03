76e28b69e61b54fd623ed0250fd2f82fd1fe3988
Xavier Belanche <xbelanch@protonmail.com>
2021-08-03 11:36:58 +0200

    [15] Hello darkness my old friend

---
+++ 15-C11/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv[0];
+
+    char *input_sample_puzzle = "0,3,6";
+
+    fprintf(stdout, "Sample puzzle: %s\n", input_sample_puzzle);
+    return 0;
+}
