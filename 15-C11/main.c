49b4e29b34250c2517c54b165f4035e4d43af823
Xavier Belanche <xbelanch@protonmail.com>
2021-08-11 22:00:58 +0200

    [15] Rewrite solution from Rossetta Code's Van Eck Sequence

--- 15-C11/main.c
+++ 15-C11/main.c
@@ -1,166 +1,79 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <wchar.h>
 
-#define MAX_TURNS_PART_ONE 2020
-#define MAX_TURNS_PART_TWO 30000000
-
-typedef struct {
-    size_t turn;
-    size_t value;
-} Number;
-
-typedef struct {
-    size_t value;
-    size_t turns[2];
+typedef struct _startingnumbers {
+    size_t *numbers;
     size_t len;
-} Turns;
+} StartingNumbers;
 
-typedef struct {
-    Turns number[2048 * 2048];
-    size_t len;
-} NumberPart2;
+size_t starting_numbers[8][16] = {
+    {0,3,6, -1},
+    {1,3,2, -1},
+    {2,1,3, -1},
+    {1,2,3, -1},
+    {2,3,1, -1},
+    {3,2,1, -1},
+    {3,1,2, -1},
+    {20,0,1,11,6,3, -1},
+};
 
-// Number numbers[MAX_TURNS];
-NumberPart2 nums;
-
-void LOG_PART_TWO() {
-    for (size_t i = 0; i < nums.len; ++i) {
-        fprintf(stdout, "%lu: ", nums.number[i].value);
-        for (size_t j = 0; j < nums.number[i].len; ++j) {
-            fprintf(stdout, "%lu, ", nums.number[i].turns[j]);
-        }
-        printf("\n");
-    }
-}
+StartingNumbers *startingNumbers[8];
+size_t lenStartingNumbers = 0;
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv[0];
+    (void) argc;
+    (void) argv[0];
 
-    // Initialize the puzzle
-    // char *input_sample_puzzle = "20,0,1,11,6,3";
-    char *input_sample_puzzle = "3,1,2";
-    char *ptr = input_sample_puzzle;
-    char *start = input_sample_puzzle;
-    size_t turns = 0;
-    char *val = malloc(sizeof(char) * 32);
-    val[0] = '\0';
-    size_t len = 0;
-    while (*ptr != '\0') {
-        if (*ptr != ',') {
-            len++;
-        } else {
-            strncpy(val, start, len);
-            val[len] = '\0';
-            start = ptr + 1;
-            nums.number[turns].value = atoll(val);
-            nums.number[turns].turns[0] = turns + 1;
-            nums.number[turns].len++;
-            nums.len++;
+    const size_t max = 30 * 1000 * 1000;
+    // const size_t limit = 10;
+    size_t *a = malloc(max * sizeof(size_t));
+    size_t *hashmap = malloc((max + 100) * sizeof(size_t));
 
-            val[0] = '\0';
-            len = 0;
-            turns++;
-        }
-        ptr++;
-    }
+    for (size_t i = 0; i < 8; ++i) {
+        if ((size_t)-1 != starting_numbers[i][0]) {
+            startingNumbers[i] = malloc(sizeof(StartingNumbers));
+            startingNumbers[i]->numbers = malloc(32 * sizeof(size_t));
+            lenStartingNumbers++;
 
-    // add last data of the input puzzle
-    strncpy(val, start, len);
-    nums.number[turns].value = atoll(val);
-    nums.number[turns].turns[0] = turns + 1;
-    nums.number[turns].len++;
-    nums.len++;
-
-    turns++;
-    free(val);
-
-    fprintf(stdout, "Sample puzzle: %s\n", input_sample_puzzle);
-
-    size_t cursor = turns - 1;
-    int found = 0;
-    int zero = -1;
-    size_t solution = 0;
-    while (turns < MAX_TURNS_PART_TWO) {
-        // first time we found that number
-        // fprintf(stdout, "Cursor: %lu | with len: %lu | turns: %lu\n",
-        //         cursor,
-        //         nums.number[cursor].len,
-        //         turns);
-        if (nums.number[cursor].len == 1) {
-            // next number is zero
-            // add it to its array of turns
-            for (size_t i = 0; i < nums.len; ++i) {
-                if (nums.number[i].value == 0) {
-                    zero = i;
-                    size_t len = nums.number[i].len;
-                    if (len > 1) {
-                        nums.number[i].turns[0] = nums.number[i].turns[1];
-                        nums.number[i].turns[1] = turns + 1;
-                    } else {
-                        nums.number[i].turns[len] = turns + 1;
-                        nums.number[i].len++;
-                    }
-                    cursor = i;
-                }
+            size_t j = 0;
+            while ((size_t)(-1) != starting_numbers[i][j]) {
+                startingNumbers[i]->numbers[j] =starting_numbers[i][j];
+                j++;
             }
-
-            if (zero < 0) {
-                printf("Create zero\n");
-                nums.number[nums.len].value = 0;
-                nums.number[nums.len].turns[0] = turns + 1;
-                nums.number[nums.len].len++;
-                cursor = nums.len;
-                zero = nums.len;
-                nums.len++;
-            }
-        } else {
-            size_t len = nums.number[cursor].len;
-            size_t delta = nums.number[cursor].turns[len - 1] - nums.number[cursor].turns[len - 2];
-            // printf("delta: %lu turns: %lu cursor: %lu\n", delta, turns, cursor);
-            for (size_t i = 0; i < nums.len; ++i) {
-                // new number!
-                if (nums.number[i].value == delta) {
-                    size_t len = nums.number[i].len;
-                    if (len > 1) {
-                        nums.number[i].turns[0] = nums.number[i].turns[1];
-                        nums.number[i].turns[1] = turns + 1;
-                    } else {
-                        nums.number[i].turns[len] = turns + 1;
-                        nums.number[i].len++;
-                    }
-
-                    cursor = i;
-                    found = 1;
-                    if (turns + 1 == MAX_TURNS_PART_TWO) {
-                        solution = nums.number[i].value;
-                    }
-                    break;
-                }
-            }
-            if (!found) {
-                // printf("Found new number (%lu)!\n", delta);
-                nums.number[nums.len].value = delta;
-                nums.number[nums.len].turns[0] = turns + 1;
-                if (turns + 1 == MAX_TURNS_PART_TWO) {
-                    solution = delta;
-                }
-                nums.number[nums.len].len++;
-                nums.len++;
-                cursor = nums.len - 1;
-            }
-        }
-        found = 0;
-        turns++;
-        if (turns % 5000000 == 0) {
-            printf("5000000...\n");
+            startingNumbers[i]->len = j;
         }
     }
-    // LOG_PART_TWO();
 
-    fprintf(stdout, "Solution for part One: %lu\n", solution /* nums.number[nums.len-1].value */ );
+    for (size_t i = 0; i < lenStartingNumbers; ++i) {
 
-    return 0;
+        for (size_t k = 0; k < max; ++k) {
+            a[k] = -1;
+        }
+        // initialize hashmap
+        for (size_t k = 0; k < (max + 100); ++k) {
+            hashmap[k] = -1;
+        }
+
+        for (size_t j = 0; j < startingNumbers[i]->len; ++j) {
+            a[j] = startingNumbers[i]->numbers[j];
+            hashmap[a[j]] = j;
+        }
+
+        for (size_t n = startingNumbers[i]->len - 1; n < max - 1; n++) {
+            if (hashmap[a[n]] != (size_t)-1) {
+                a[n + 1] = n - hashmap[a[n]];
+                hashmap[a[n]] = n;
+            } else {
+                a[n + 1] = 0;
+                hashmap[a[n]] = n;
+            }
+        }
+
+        fprintf(stdout, "Solution for Day 15: %lu\n", a[max - 1]);
+
+    }
+    free(a);
+    free(hashmap);
 }
