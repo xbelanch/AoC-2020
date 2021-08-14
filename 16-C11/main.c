ac25360785b5f19b4bb110205fc7874709ac8136
Xavier Belanche <xbelanch@protonmail.com>
2021-08-14 11:05:24 +0200

    Parse and clean input files

--- 16-C11/main.c
+++ 16-C11/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int isNumber(char c) {
+    return (c >= '0' && c <= '9') ? 1 : 0;
+}
+
 int parse_file(char *filename) {
     fprintf(stdout, "Input file: %s\n", filename);
 
@@ -11,17 +15,42 @@ int parse_file(char *filename) {
     }
 
     int c = getc(fp);
+    int lastc = c;
+    size_t cursor = 0;
+    size_t colon = 0;
     size_t lines = 0;
     while (c != EOF) {
-        if (c == '\n') {
+        if (isNumber(lastc) && c == '\n') {
+            fprintf(stdout, "\n");
+            cursor = 0;
+            colon = 0;
+            lines++;
+        } else if (cursor >= 8 && lastc == 't' && c == ':') {
+            fprintf(stdout, "---\n");
+        } else if (cursor >= 10 && lastc == 's' && c == ':') {
+            fprintf(stdout, "---\n");
+        } else if (isNumber(c)) {
+            fprintf(stdout, "%c", c);
+        } else if (c == '-') {
+            fprintf(stdout, "%c", c);
+        } else if (c == ',') {
+            fprintf(stdout, "%c", c);
+        } else if (colon && lastc == 'o' && c == 'r') {
+            fprintf(stdout, " || ");
+        } else if (c == ':') {
+            colon = 1;
+        } else if (c == '\n') {
+            cursor = 0;
             lines++;
         } else {
-            // fprintf(stdout, "%c", c);
+
         }
+        cursor++;
+        lastc = c;
         c = getc(fp);
     }
 
-    fprintf(stdout, "Number of lines: %lu\n", lines);
+    fprintf(stdout, "\nNumber of lines: %lu\n", lines);
     fclose(fp);
 
     return 0;
