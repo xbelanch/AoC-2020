7eab756c273ee9c18b6db506ea871a3b20224c39
Xavier Belanche <xbelanch@protonmail.com>
2021-08-23 08:01:27 +0200

    [16] Collect the "your ticket" array of numbers

--- 16-C11/main.c
+++ 16-C11/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 typedef struct {
+    size_t position;
     size_t min;
     size_t max;
 } Range;
@@ -12,6 +13,8 @@ size_t srange;
 size_t **nearby_tickets;
 size_t row;
 size_t sz_nearby_tickets;
+size_t *myticket;
+size_t sz_myticket;
 
 int isDigit(char c) {
     return (c >= '0' && c <= '9') ? (1) : (0);
@@ -53,13 +56,18 @@ int parse_input_file(char *filename) {
     size_t snumber = 0;
     range = malloc(sizeof(Range) * 128);
     srange = 0;
+
     row = 1;
+
     nearby_tickets = (size_t**)malloc(sizeof(size_t) * 1024);
     for (size_t i = 0; i < 1024; ++i) {
         nearby_tickets[i] = (size_t*)malloc(sizeof(size_t) * 20);
     }
-
     sz_nearby_tickets = 0;
+
+    myticket = (size_t*)malloc(sizeof(size_t) * 32);
+    sz_myticket = 0;
+
     while (c != EOF) {
         if (isNewLine(c) && isNewLine(lastChar))
             entrySection++;
@@ -88,7 +96,21 @@ int parse_input_file(char *filename) {
         }
 
         if (entrySection == 1) {
-            // Oh...
+            // ... do something myticket
+            if (isDigit(c)) {
+                number[snumber++] = c;
+                number[snumber] = '\0';
+            } else if ((isSymbol(c, ',')) && (isDigit(lastChar))) {
+                myticket[sz_myticket] = atoll(number);
+                number[0] = '\0';
+                snumber = 0;
+                sz_myticket++;
+            } else if (isNewLine(c) && isDigit(lastChar)) {
+                myticket[sz_myticket] = atoll(number);
+                number[0] = '\0';
+                snumber = 0;
+                sz_myticket++;
+            }
         }
 
         if (entrySection == 2) {
@@ -116,13 +138,22 @@ int parse_input_file(char *filename) {
     return (0);
 }
 
+size_t solutionPartTwo() {
+    size_t solution = 0;
+    for (size_t i = 0; i < sz_myticket; ++i) {
+        fprintf(stdout, "%lu ", myticket[i]);
+    }
+
+    return solution;
+}
+
 size_t solutionPartOne() {
     size_t solution = 0;
     size_t notinrange = 0;
 
     for (size_t j = 0; j < (row - 1); ++j) {
         for (size_t i = 0; i < sz_nearby_tickets; ++i) {
-            // fprintf(stdout, "%lu ", nearby_tickets[j][i]);
+            fprintf(stdout, "%lu ", nearby_tickets[j][i]);
             for (size_t k = 0; k < srange; ++k) {
                 if (inRange(range[k], nearby_tickets[j][i])) {
                     break;
@@ -146,8 +177,9 @@ int main(int argc, char *argv[])
     (void) argc;
     (void) argv[0];
 
-    char *input_file = "input.txt";
+    char *input_file = "sample-input2.txt";
     parse_input_file(input_file);
     fprintf(stdout, "Solution for Part One: %lu\n", solutionPartOne());
+    fprintf(stdout, "Solution for Part Two: %lu\n", solutionPartTwo());
     return (0);
 }
