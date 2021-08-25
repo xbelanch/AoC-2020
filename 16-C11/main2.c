dda17c6141175bef3db2ce05c8e8bb2b072d7144
Xavier Belanche <xbelanch@protonmail.com>
2021-08-25 14:13:14 +0200

    [16] Input data is now flawlessly parsed

--- 16-C11/main2.c
+++ 16-C11/main2.c
@@ -21,11 +21,18 @@ typedef struct {
 typedef struct {
     size_t *numbers;
     size_t len;
-} MyTicket;
+} Ticket;
+
+typedef struct {
+    Ticket *ticket;
+    size_t len;
+} Nearby;
 
 InputTextFile inputTextFile;
 Rule rule;
-MyTicket myTicket;
+Ticket myTicket;
+Nearby nearby;
+
 
 int inRange(Range range, size_t number) {
     return number >= range.min && number <= range.max ? (1) : (0);
@@ -33,11 +40,14 @@ int inRange(Range range, size_t number) {
 
 int parse_ranges_field(Field *field, char *input) {
     int last;
-    size_t range = 0, minmatch = 0, maxmatch = 0, input_len = strlen(input);
+    size_t range = 0;
+    size_t minmatch, maxmatch;
+    size_t input_len = strlen(input);
     field->name = (char*)malloc(sizeof(char) * 128);
     char *number = malloc(sizeof(char) * 16);
     size_t len = 0;
     for (size_t i = 0; i < input_len; ++i) {
+        // putchar(input[i]);
         if (isSymbol(input[i], ':')) memcpy(field->name, input, i);
         if (isDigit(input[i]) && isSpace(last)) minmatch = 1;
         if (isDigit(input[i]) && isSymbol(last, '-')) maxmatch = 1;
@@ -61,7 +71,7 @@ int parse_ranges_field(Field *field, char *input) {
     return(0);
 }
 
-int parse_ticket(MyTicket *myTicket, char *input) {
+int parse_Ticket(Ticket *myTicket, char *input) {
     myTicket->numbers = (size_t*)malloc(sizeof(size_t) * 128);
     myTicket->len = 0;
     size_t len_input = strlen(input);
@@ -71,8 +81,43 @@ int parse_ticket(MyTicket *myTicket, char *input) {
         if (isDigit(input[i])) number[len++] = input[i];
         if (isSymbol(input[i], ',') || i == len_input - 1) {
             myTicket->numbers[myTicket->len++] = atoll(number);
+            number[len - 1] = '\0';
             len = 0;
         }
+    }
+    return (0);
+}
+
+int print_field_ranges(Rule rule, size_t i) {
+    fprintf(stdout, "%s: %lu-%lu %lu-%lu", rule.fields[i].name,
+            rule.fields[i].ranges[0].min,
+            rule.fields[i].ranges[0].max,
+            rule.fields[i].ranges[1].min,
+            rule.fields[i].ranges[1].max);
+    return(0);
+}
+
+int print_parsed_data(void) {
+    // Print all the field ranges
+    for (size_t i = 0; i < rule.len; ++i) {
+        print_field_ranges(rule, i);
+        putchar('\n');
+    }
+    putchar('\n');
+
+    // Print your ticket
+    for (size_t i = 0; i < myTicket.len; ++i) {
+        fprintf(stdout, "%lu ", myTicket.numbers[i]);
+    }
+    putchar('\n');
+    putchar('\n');
+
+    // Print nearby tickets
+    for (size_t i = 0; i < nearby.len; ++i) {
+        for (size_t j = 0; j < nearby.ticket[i].len; ++j) {
+            fprintf(stdout, "%lu ", nearby.ticket[i].numbers[j]);
+        }
+        putchar('\n');
     }
     return (0);
 }
@@ -91,6 +136,9 @@ int parse_input_file(char *input_filename) {
     rule.fields = (Field*)malloc(sizeof(Field) * 32);
     rule.len = 0;
 
+    nearby.ticket = (Ticket*)malloc(sizeof(Ticket) * 512);
+    nearby.len = 0;
+
     size_t section = 0;
     for (size_t i = 0; i < inputTextFile.size; ++i) {
         if (!strcmp("your ticket:", inputTextFile.lines[i]) || !strcmp("nearby tickets:", inputTextFile.lines[i])) {
@@ -99,20 +147,20 @@ int parse_input_file(char *input_filename) {
             switch(section) {
             case 0:
                 parse_ranges_field(&(rule.fields)[rule.len], inputTextFile.lines[i]);
-                rule.len = i;
+                rule.len++;
                 break;
             case 1:
-                // TODO: Parse "my" ticket
-                parse_ticket(&myTicket, inputTextFile.lines[i]);
+                parse_Ticket(&myTicket, inputTextFile.lines[i]);
                 break;
             case 2:
-                // TODO: Parse nearby tickets
+                parse_Ticket(&(nearby.ticket[nearby.len++]), inputTextFile.lines[i]);
                 break;
             default:
                 break;
             }
         }
     }
+
 
     return (0);
 }
@@ -124,17 +172,7 @@ int main(int argc, char *argv[])
 
     char *input_filename = "input.txt";
     parse_input_file(input_filename);
-
-    // print all the ranges
-    // for (size_t i = 0; i < rule.len; ++i) {
-    //     fprintf(stdout, "%s: %lu-%lu %lu-%lu", rule.fields[i].name,
-    //             rule.fields[i].ranges[0].min,
-    //             rule.fields[i].ranges[0].max,
-    //             rule.fields[i].ranges[1].min,
-    //             rule.fields[i].ranges[1].max);
-    //     putchar('\n');
-    // }
-
+    print_parsed_data();
 
     return (0);
 }
